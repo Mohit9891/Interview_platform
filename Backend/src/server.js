d7684110e5836ce0e3b014c2649e85daa34e87ab
Mohit <mohitprajapati1412009@gmail.com>
2026-01-08 09:54:04 +0530

    auth

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,10 +3,22 @@ import { ENV } from "./lib/env.js";
 // import { fileURLToPath } from "url";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import{serve} from "inngest/express"
+import { functions, inngest } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json());
+// app.use(cors()) // leave as it is in development
+// Credentials:true => SERVER allow a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true})) // for production
+
+app.use("/api/inngest", serve({client:inngest} , functions));
+
 
 app.get("/", (req, res) => {
   res.send("server running");
